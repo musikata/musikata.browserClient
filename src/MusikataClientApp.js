32c01c44debe68488fb27f057b2be405c70d2f26
adorsk <adorsk@gmail.com>
2014-04-07 10:33:17 -0400

    initial sketching, stubbing

--- src/MusikataClientApp.js
+++ src/MusikataClientApp.js
@@ -1,5 +1,36 @@
 define(function(require){
-  var MusikataClientApp = {};
+  var _ = require('underscore');
+
+  var MusikataClientApp = function(){
+    // Dummy db for paths. This will probably change.
+    this.paths = {};
+  };
+
+  _.extend(MusikataClientApp.prototype, {
+
+    showPathNode: function(pathId, nodePath){
+      // Get the path from the db.
+      var path = this.paths[pathId];
+
+      // Get the node from the path.
+      var node = path.getNodeByPath(nodePath);
+
+      // Create the view for the node.
+      var nodeView = this.createViewForNode(node);
+
+      // Show the view.
+      this.showView(nodeView);
+    },
+
+    createViewForNode: function(node){
+      return {};
+    },
+
+    showView: function(view){
+      return;
+    }
+
+  });
 
   return MusikataClientApp;
 });
--- test/specs/MusikataClientApp.spec.js
+++ test/specs/MusikataClientApp.spec.js
@@ -1,9 +1,60 @@
 define(function(require) {
+
+  var _ = require('underscore');
+
   describe('MusikataClientApp', function(){
     var MusikataClientApp = require('musikata_browser_client/MusikataClientApp');
 
     it('should be defined', function() {
       expect(MusikataClientApp).toBeDefined();
     })
+
+    xdescribe("when path route is triggered", function(){
+      it("should fire show_path function", function(){
+        this.fail('NOT IMPLEMENTED');
+      });
+    });
+
+    describe("showPathNode", function(){
+      var app;
+
+      // Mock path object.
+      var Path = function(data){
+        this.data = data;
+      };
+      _.extend(Path.prototype, {
+        // For now, just return dummy node.
+        getNodeByPath: function(nodePath){
+          return {id: 'dummyNode'};
+        }
+      });
+
+      var testPath = new Path({
+        id: 'testPath',
+        children: [
+          {
+            id: 'A',
+            children: [
+              {id: 'A.A'}
+            ]
+          }
+        ]
+      });
+
+      beforeEach(function(){
+        app = new MusikataClientApp();
+        app.paths = {
+          'testPath': testPath
+        };
+      });
+
+      it("should show view for specified node", function(){
+        var pathId = 'testPath';
+        var nodePath = '/A/A.A';
+        app.showPathNode(pathId, nodePath);
+        expect(app.currentView).toBe(viewForNode);
+      });
+    });
+
   });
 });
